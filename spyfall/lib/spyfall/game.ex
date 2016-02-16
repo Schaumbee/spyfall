3d3be8271ec3b8bb2cf614619c3e5f3ac05dd8e6
Jonathan Como <jonathan.como@gmail.com>
2016-02-15 16:44:24 -0800

    Finish the logic so that the game is able to be completed

--- spyfall/lib/spyfall/game.ex
+++ spyfall/lib/spyfall/game.ex
@@ -1,24 +1,25 @@
 defmodule Spyfall.Game do
   use GenServer
 
+  @min_players Application.get_env(:spyfall, :min_players)
+  @locations Application.get_env(:spyfall, :locations)
+
   ## Client API
 
   def start_link(player_names) do
-    min = Application.get_env(:spyfall, :min_players)
-
-    if length(player_names) < min do
-      {:error, "At least #{min} players are needed to start the game"}
+    if length(player_names) < @min_players do
+      {:error, "At least #{@min_players} players are needed to start the game"}
     else
       GenServer.start_link(__MODULE__, player_names)
     end
   end
 
-  def spy(game) do
-    GenServer.call(game, :spy)
+  def guess_spy(game, player, guess) do
+    GenServer.call(game, {:guess_spy, player, guess})
   end
 
-  def location(game) do
-    GenServer.call(game, :location)
+  def guess_location(game, player, guess) do
+    GenServer.call(game, {:guess_location, player, guess})
   end
 
   def players(game) do
@@ -32,14 +33,47 @@ defmodule Spyfall.Game do
   ## Server (callback)
 
   def init(player_names) do
-    {location, roles} = random_location
+    {location, roles} = Enum.random(@locations)
     players = assign_roles(player_names, roles)
 
     {:ok, {location, players}}
   end
 
-  def handle_call(:location, _from, {location, _} = state) do
-    {:reply, location, state}
+  def handle_call({:guess_spy, player, guess}, _from, {_, players} = state) do
+    spy_player = spy(players)
+
+    reply = cond do
+      player == spy_player ->
+        {:error, "The spy cannot guess the spy"}
+      guess == spy_player ->
+        {:ok, :correct, spy_player}
+      Map.has_key?(players, guess) ->
+        {:ok, :incorrect, spy_player}
+      true ->
+        {:error, "#{guess} is not playing the game"}
+    end
+
+    {:reply, reply, state}
+  end
+
+  def handle_call({:guess_location, player, guess}, _from, {location, players} = state) do
+    spy_player = spy(players)
+    valid_location? = Enum.any?(@locations, fn {name, _} ->
+      String.downcase(name) == String.downcase(guess)
+    end)
+
+    reply = cond do
+      player != spy_player ->
+        {:error, "Only the spy can guess the location"}
+      String.downcase(guess) == String.downcase(location) ->
+        {:ok, :correct, location}
+      valid_location? ->
+        {:ok, :incorrect, location}
+      true ->
+        {:error, "#{guess} is not a valid location"}
+    end
+
+    {:reply, reply, state}
   end
 
   def handle_call(:players, _from, {_, players} = state) do
@@ -65,16 +99,16 @@ defmodule Spyfall.Game do
     Map.put(assign_non_spy_roles(rest, roles), spy, :spy)
   end
 
-  defp random_location do
-    locations = Application.get_env(:spyfall, :locations)
-    Enum.random(locations)
-  end
-
   defp assign_non_spy_roles(names, roles) do
     players_and_roles = Enum.zip(names, Stream.cycle(roles))
 
     Enum.reduce(players_and_roles, %{}, fn {name, role}, acc ->
       Map.put(acc, name, role)
     end)
+  end
+
+  defp spy(players) do
+    {name, _} = Enum.find(players, fn {_, role} -> role == :spy end)
+    name
   end
 end
--- spyfall/lib/spyfall/game_loop.ex
+++ spyfall/lib/spyfall/game_loop.ex
@@ -1,12 +1,15 @@
 defmodule Spyfall.GameLoop do
   use GenServer
 
+  @location_guess ~r/^location:\s*(?<location>[a-zA-Z ]+)$/i
+  @spy_guess ~r/^(?<spy>\w+) is the spy!?$/i
+
   def start_link do
     GenServer.start_link(__MODULE__, :ok)
   end
 
   def respond(game, player, message) do
-    if message =~ ~r/^start$/ do
+    if message =~ ~r/^start$/i do
       GenServer.call(game, :start)
     else
       GenServer.call(game, {:message, player, message})
@@ -37,19 +40,19 @@ defmodule Spyfall.GameLoop do
 
   def handle_call({:message, player, message}, _from, {:lobby, lobby} = state) do
     reply = cond do
-      message =~ ~r/^lobby$/ ->
+      message =~ ~r/^lobby$/i ->
         case Spyfall.Lobby.players(lobby) do
           [] ->
             "No one is waiting to play the next game"
           players ->
             "Here are the players waiting for the next game: #{Enum.join(players, ", ")}"
         end
-      message =~ ~r/^join$/ ->
+      message =~ ~r/^join$/i ->
         case Spyfall.Lobby.join(lobby, player) do
           :ok             -> "#{player} has joined the game"
           {:error, error} -> error
         end
-      message =~ ~r/^leave$/ ->
+      message =~ ~r/^leave$/i ->
         case Spyfall.Lobby.leave(lobby, player) do
           :ok             -> "#{player} has left the game"
           {:error, error} -> error
@@ -63,10 +66,48 @@ defmodule Spyfall.GameLoop do
   end
 
   def handle_call({:message, player, message}, _from, {:game, game} = state) do
-    {:reply, [{:broadcast, "In the game"}], state}
+    cond do
+      message =~ @location_guess ->
+        guess = Regex.named_captures(@location_guess, message)["location"]
+        guess_location(state, player, guess)
+      message =~ @spy_guess ->
+        guess = Regex.named_captures(@spy_guess, message)["spy"]
+        guess_spy(state, player, guess)
+      true ->
+        {:reply, [], state}
+    end
   end
 
   def handle_call(:start, _from, {:game, game} = state) do
     {:reply, [{:broadcast, "There is already a game being played"}], state}
+  end
+
+  defp guess_location({:game, game} = state, player, guess) do
+    case Spyfall.Game.guess_location(game, player, guess) do
+      {:ok, :correct, _} ->
+        finish_game("Good work, Agent #{player}! I knew we could count on you.")
+      {:ok, :incorrect, location} ->
+        finish_game("You blew your cover, Agent #{player}! The correct location was: #{location}")
+      {:error, msg} ->
+        IO.puts "ERROR: #{msg}"
+        {:reply, [], state}
+    end
+  end
+
+  defp guess_spy({:game, game} = state, player, guess) do
+    case Spyfall.Game.guess_spy(game, player, guess) do
+      {:ok, :correct, name} ->
+        finish_game("You're right! #{name} is the spy!")
+      {:ok, :incorrect, name} ->
+        finish_game("You're wrong! #{guess} isn't the spy; #{name} is the spy!")
+      {:error, msg} ->
+        IO.puts "ERROR: #{msg}"
+        {:reply, [], state}
+    end
+  end
+
+  defp finish_game(message) do
+    {:ok, new_lobby} = Spyfall.Lobby.start_link
+    {:reply, [{:broadcast, message}], {:lobby, new_lobby}}
   end
 end
